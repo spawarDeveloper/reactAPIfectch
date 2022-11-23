fc1ef387d0c369b8c05f2aea0a1a3e1d938c0b73
spawarDeveloper <sriniva.pawar57@gmail.com>
2022-11-23 16:06:26 +0530

    react js api call with fetch method

---
+++ restapi/src/App.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+function App() {
+  const [user, setUser] = useState();
+
+  const userData = () => {
+    return fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  };
+
+  useEffect(() => {
+    userData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>REACT JS API with fetch method</h1>
+      <ul>
+        {user &&
+          user.length > 0 &&
+          user.map((item, i) => <li key={item.id}>{item.name}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
